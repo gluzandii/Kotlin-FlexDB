232c7fa56a10a9174f11379793d4fba014fa69d5
Sushant Nariangadu <nsushant2007@gmail.com>
2022-10-24 10:38:44 +0530

    Setup rest controller.
    More:
    + Add kotlinx.coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/org/solo/kotlin/flexdb/FlexDbApplication.kt
+++ src/main/kotlin/org/solo/kotlin/flexdb/FlexDbApplication.kt
@@ -2,10 +2,25 @@ package org.solo.kotlin.flexdb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FlexDbApplication
 
 fun main(args: Array<String>) {
     runApplication<FlexDbApplication>(*args)
+}
+
+@RestController
+@Suppress("unused")
+class RestControl {
+    @GetMapping("/")
+    fun root() = mapOf(Pair("status", "OK"))
+
+    @PostMapping("/")
+    fun query() {
+
+    }
 }
