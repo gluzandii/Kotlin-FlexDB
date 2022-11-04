632bda6cc27fd9602e60d984614ea0bb2893984d
Sushant Nariangadu <nsushant2007@gmail.com>
2022-11-04 21:35:39 +0530

    Refactoring

--- src/main/kotlin/org/solo/kotlin/flexdb/db/query/clause/Where.kt
+++ src/main/kotlin/org/solo/kotlin/flexdb/db/query/clause/Where.kt
@@ -1,10 +1,10 @@
 package org.solo.kotlin.flexdb.db.query.clause
 
 import org.solo.kotlin.flexdb.InvalidTypeException
-import org.solo.kotlin.flexdb.db.query.operators.RelationalOperators
+import org.solo.kotlin.flexdb.db.query.operators.RelationalOperator
 import org.solo.kotlin.flexdb.db.types.DbValue
 
-data class Where(val one: DbValue<*>, val two: DbValue<*>, val op: RelationalOperators) {
+data class Where(val one: DbValue<*>, val two: DbValue<*>, val op: RelationalOperator) {
     init {
         if (!one.typeEquals(two)) {
             throw InvalidTypeException("Db value one and two do not have matching types.")
