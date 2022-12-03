d3c028926d08b6008a11f6d17b01bc94b6b42166
Sushant Nariangadu <nsushant2007@gmail.com>
2022-12-03 14:44:25 +0530

    Fix the error where it would not register the 'query' route.
    More:
    + Made error handling more clean in the query route.

--- src/main/kotlin/org/solo/kotlin/flexdb/plugins/Routing.kt
+++ src/main/kotlin/org/solo/kotlin/flexdb/plugins/Routing.kt
@@ -1,59 +1,14 @@
 package org.solo.kotlin.flexdb.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.solo.kotlin.flexdb.json.JsonUtil
-import org.solo.kotlin.flexdb.json.query.JsonQueryTypes
-import org.solo.kotlin.flexdb.json.query.JsonQueryUtil
-import org.solo.kotlin.flexdb.json.query.classes.*
 
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
             call.respond(mapOf("status" to "OK"))
-        }
-
-        authenticate("query") {
-            post("/query") {
-                // Structure of queries has been put in the "when" statement
-                // It will be implemented at the end.
-
-                try {
-                    val body = call.receiveText()
-                    val mapper = JsonUtil.newObjectMapper()
-
-                    @Suppress("UNUSED_VARIABLE")
-                    when (JsonQueryUtil.getQueryType(body)) {
-                        JsonQueryTypes.CREATE -> {
-                            val query = mapper.readValue(body, JsonCreateQuery::class.java)!!
-                        }
-
-                        JsonQueryTypes.SELECT -> {
-                            val query = mapper.readValue(body, JsonSelectQuery::class.java)!!
-                        }
-
-                        JsonQueryTypes.UPDATE -> {
-                            val query = mapper.readValue(body, JsonUpdateQuery::class.java)!!
-                        }
-
-                        JsonQueryTypes.DELETE -> {
-                            val query = mapper.readValue(body, JsonDeleteQuery::class.java)!!
-                        }
-
-                        JsonQueryTypes.RESET -> {
-                            val query = mapper.readValue(body, JsonResetQuery::class.java)!!
-                        }
-                    }
-                } catch (ex: Exception) {
-                    System.err.println("An error occurred:")
-                    ex.printStackTrace()
-                }
-            }
         }
     }
 }
--- src/main/kotlin/org/solo/kotlin/flexdb/plugins/Security.kt
+++ src/main/kotlin/org/solo/kotlin/flexdb/plugins/Security.kt
@@ -1,18 +1,118 @@
 package org.solo.kotlin.flexdb.plugins
 
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.solo.kotlin.flexdb.json.JsonUtil
+import org.solo.kotlin.flexdb.json.query.JsonQueryTypes
+import org.solo.kotlin.flexdb.json.query.JsonQueryUtil
+import org.solo.kotlin.flexdb.json.query.classes.*
+
+suspend inline fun handleError(ex: Throwable, call: ApplicationCall) {
+    val msg: String
+
+    when (ex) {
+        is JsonParseException -> {
+            msg = "Couldn't parse query: ${ex.message}"
+            ex.printStackTrace()
+
+            call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = mapOf(
+                    "status" to "could not parse query",
+                    "message" to ex.message,
+                    "stack trace" to ex.stackTraceToString()
+                )
+            )
+        }
+
+        is MismatchedInputException -> {
+            msg = "Invalid query: ${ex.message}"
+            ex.printStackTrace()
+
+            call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = mapOf(
+                    "status" to "bad query",
+                    "message" to ex.message,
+                    "stack trace" to ex.stackTraceToString()
+                )
+            )
+        }
+
+        else -> {
+            msg = "An error occurred: ${ex.message}"
+            ex.printStackTrace()
+
+            call.respond(
+                status = HttpStatusCode.InternalServerError,
+                message = mapOf(
+                    "status" to "error",
+                    "message" to ex.message,
+                    "stack trace" to ex.stackTraceToString()
+                )
+            )
+        }
+    }
+
+    System.err.println(msg)
+}
 
 fun Application.configureSecurity() {
-
-    authentication {
-        basic(name = "query") {
-            realm = "Ktor Server"
+    install(Authentication) {
+        basic(name = "flexdb-query") {
+            realm = "flexdb"
             validate { credentials ->
-                if (credentials.name == credentials.password) {
+                if (credentials.name == "admin" && credentials.password == "admin") {
                     UserIdPrincipal(credentials.name)
                 } else {
                     null
+                }
+            }
+        }
+    }
+
+    routing {
+        authenticate("flexdb-query") {
+            post("/query") {
+                // Structure of queries has been put in the "when" statement
+                // It will be implemented at the end.
+
+                try {
+                    val body = call.receiveText()
+                    val mapper = JsonUtil.newObjectMapper()
+
+                    @Suppress("UNUSED_VARIABLE")
+                    when (JsonQueryUtil.getQueryType(body)) {
+                        JsonQueryTypes.CREATE -> {
+                            val query = mapper.readValue(body, JsonCreateQuery::class.java)!!
+                        }
+
+                        JsonQueryTypes.SELECT -> {
+                            val query = mapper.readValue(body, JsonSelectQuery::class.java)!!
+                        }
+
+                        JsonQueryTypes.UPDATE -> {
+                            val query = mapper.readValue(body, JsonUpdateQuery::class.java)!!
+                        }
+
+                        JsonQueryTypes.DELETE -> {
+                            val query = mapper.readValue(body, JsonDeleteQuery::class.java)!!
+                        }
+
+                        JsonQueryTypes.RESET -> {
+                            val query = mapper.readValue(body, JsonResetQuery::class.java)!!
+                        }
+                    }
+
+                    call.respond(mapOf("status" to "ok"))
+                } catch (ex: Throwable) {
+                    handleError(ex, call)
                 }
             }
         }
